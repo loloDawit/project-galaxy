5d08763949326636703115a4f6e191a73ed9ff7e
Dawit Abera <dawit.y.abera@nordstrom.com>
2021-03-19 16:32:24 -0700

    removing db uri

--- db.js
+++ db.js
@@ -1,18 +1,15 @@
-const mongoose = require("mongoose");
-const Note = require("./model/Notes");
+const mongoose = require('mongoose');
+const Note = require('./model/Notes');
 
-mongoose.connect(
-  "mongodb+srv://dev-user:newUser2021@seattle.eqx8h.mongodb.net/test",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const coding101 = new Note({
-  Title: "Introduction to Github",
-  Description: "learn how to use Github effectively",
+  Title: 'Introduction to Github',
+  Description: 'learn how to use Github effectively',
   Reminder: true,
 });
 
-coding101.save().then(() => console.log("The data have been saved."));
+coding101.save().then(() => console.log('The data have been saved.'));
